aa030e315ef70ea6d568a3b1be8e9fd7caa0595f
sheikanas <sheikanasa@ilink-systems.com>
2018-01-04 11:53:51 +0530

    added jenkinsfile

---
+++ jenkinsfile.js
@@ -0,0 +1,10 @@
+Jenkinsfile (Scripted Pipeline)
+/* Requires the Docker Pipeline plugin */
+node('docker') {
+    checkout scm
+    stage('Build') {
+        docker.image('node:6.3').inside {
+            sh 'npm --version'
+        }
+    }
+}
